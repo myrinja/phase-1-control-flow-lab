d5f83180253a038557029dd00604a1527fca2be9
myrinja <myrinjau98@gmail.com>
2023-07-11 23:57:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400 ) {
+    return "This one is on me!";
+  }
+  else if(feet > 2000 && feet < 2500 ){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if(feet > 2500) {
+    return "No can do.";
+  }
+}
+// Write your code here!
+
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  }
+  else {
+    return "No go."
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
+  if (tip === "generous") {
+    return "Thank you so much." ;
+  }
+  else if (tip === "not as generous") {
+    return "Thank you.";
+  }
+  else{
+    return "Bye.";
+  }
 }
